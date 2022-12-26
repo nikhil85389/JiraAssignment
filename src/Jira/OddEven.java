e3f9a375e09385507c00a9db531bbb2227df055d
nikhil85389 <nikhil85389@gmail.com>
2022-12-26 18:00:37 +0530

    uppdate

--- src/Jira/OddEven.java
+++ src/Jira/OddEven.java
@@ -4,12 +4,16 @@ public class OddEven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Odd Even");
 		int a =20;
 		if(a%2 == 0) {
 			System.out.println("Even");
 		}else {
 			System.out.println("Odd");
 		}
+		
+		
 
 	}
 
