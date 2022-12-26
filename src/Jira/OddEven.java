5ba076cfa5078f8307fc29e818a0e0c565b7d642
nikhil85389 <nikhil85389@gmail.com>
2022-12-26 17:15:14 +0530

    First commit

---
+++ src/Jira/OddEven.java
@@ -0,0 +1,16 @@
+package Jira;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =20;
+		if(a%2 == 0) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
